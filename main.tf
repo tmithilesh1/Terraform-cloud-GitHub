7fcd54cf3208b71589be153155c797f33538a082
tmithilesh1 <tmithilesh1110@gmail.com>
2025-07-25 08:42:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Mithilesh-infra" {
+resource "aws_instance" "Mithilesh1-infra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.nano"
 }
